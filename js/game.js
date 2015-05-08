715425e2b59af7444f36fef19c310588e03a05c6
Yeng <leeyeng2005@gmail.com>
2015-05-07 20:30:47 -0500

    working points counter

--- js/game.js
+++ js/game.js
@@ -1,7 +1,27 @@
 $(document).ready(function() {
-	$('.pictures').on('click', function() {
-		//alert('Clicked');
-		$('.popUp').find("div").removeClass("hide");
-		console.log("clicked");
+		var points = 0;
+	$('.picture').on('click', function() {
+
+		var picture = $(this);
+		var picId = picture.data("id");
+		var correctAnswer = "btp";
+	
+
+		//If img of li is clicked
+
+		//verify the data 
+		console.log(picId + "===" + correctAnswer);
+		if (picId === correctAnswer) {
+			$('#popUp').find(".correct").removeClass("hide");
+			points = points + 1;
+			console.log(points);
+
+			console.log("clicked");
+		} else { 
+			$('#popUp').find(".incorrect").removeClass("hide");
+		}
+		console.log("clicked"); 
+
+		
 	});
 });
