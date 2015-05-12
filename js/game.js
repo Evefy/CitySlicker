a46dea83aa592aa06144ec61bf7dfbbaa4ea473d
Yeng <leeyeng2005@gmail.com>
2015-05-11 19:37:55 -0500

    isCorrect and isWrong function with start of quiz objects

--- js/game.js
+++ js/game.js
@@ -1,27 +1,54 @@
 $(document).ready(function() {
-		var points = 0;
-	$('.picture').on('click', function() {
+	
+	var points = 0;
 
+	var isCorrectAnswer = function() {
+		$('#popUp').find(".correct").removeClass("hide");
+		points++;
+	}	
+
+	var isWrongAnswer = function(){
+		$('#popUp').find(".incorrect").removeClass("hide");
+	}
+
+	$('#pictures').on('click', '[data-id="btp"]', isCorrectAnswer);
+	$('#pictures').on('click', '[data-id="briewood"]', isWrongAnswer);
+	$('#pictures').on('click', '[data-id="wando"]', isWrongAnswer); 
+ 	/*
 		var picture = $(this);
 		var picId = picture.data("id");
+
 		var correctAnswer = "btp";
-	
+		
+
 
 		//If img of li is clicked
 
 		//verify the data 
-		console.log(picId + "===" + correctAnswer);
-		if (picId === correctAnswer) {
-			$('#popUp').find(".correct").removeClass("hide");
-			points = points + 1;
-			console.log(points);
+		// console.log(picId + "===" + correctAnswer);
+		// if (picId === correctAnswer) {
+		// 	$('#popUp').find(".correct").removeClass("hide");
+		// 	points++; 
 
-			console.log("clicked");
-		} else { 
-			$('#popUp').find(".incorrect").removeClass("hide");
-		}
-		console.log("clicked"); 
+		// 	var totalPoints = $(".points").find("p").append(points);
+		// 	console.log("clicked");
+		// } else { 
+		// 	$('#popUp').find(".incorrect").removeClass("hide");
+		// }
+		// console.log("clicked"); 
 
+		$('#pictures').on("click", "picture" isCorrectAnswer);
+		$('#pictures').on("click", "picture" isWrongAnswer);
 		
 	});
+	*/
+	$('button').on('click', '.next', function(){
+		$.ajax('level1.html', { 
+			success: function(response) {
+			$('#level1').html(response).slideDown();
+	 		}
+		});
+	});
+
+
 });
